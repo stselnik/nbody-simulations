d88562ffb58de70939d374afb9dc10d6c3d0fdb2
Samuel Tselnik <139260316+stselnik@users.noreply.github.com>
2025-05-17 16:32:34 -0400

    feat: Simulating Orbit with Several Entities - Currently storing all position values in array holding entity information. - Added Moon, Venus as examples (Confirmed that values from JPL Ephemeris work with simulation

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 class OrbitalEntity:
-    def __init__(self, x, y, z, vx, vy, vz, mass):
+    def __init__(self, x, y, z, vx, vy, vz, mass, ax):
         self.x = x
         self.y = y
         self.z = z
@@ -13,24 +13,28 @@ class OrbitalEntity:
         self.vy = vy
         self.vz = vz
         self.mass = mass
+        self.xpoints = np.array([])
+        self.ypoints = np.array([])
+        self.zpoints = np.array([])
+        self.ln, = ax.plot([], [], [])
+
     def __str__(self):
         return f"Position: ({self.x}, {self.y}, {self.z}), Velocity: ({self.vx}, {self.vy}, {self.vx}) , Mass: {self.mass}"
 
+fig, ax = plt.subplots()
+ax = fig.add_subplot(projection='3d')
+
 orbital_entities = []
 # Entity representing the Sun
-orbital_entities.append(OrbitalEntity(0, 0, 0, 0, 0, 0, 1.989e30))
-# Entity representing the Earth
-orbital_entities.append(OrbitalEntity(-149597871, 0, 0, 0, 29.78, 0, 5.972e24))
+orbital_entities.append(OrbitalEntity(0, 0, 0, 0, 0, 0, 1.989e30, ax))
+# Entity representing Earth
+orbital_entities.append(OrbitalEntity(-8.529440084914488E+07, -1.262070629279483E+08, 3.267467302957177E+04, 2.421505166691593E+01, -1.677450100101587E+01, 2.508322287999576E-04, 5.972e24, ax))
+# Entity representing the Moon
+#orbital_entities.append(OrbitalEntity(-8.519842463576744E+07, -1.265887274718351E+08,-2.688295040652156E+02, 2.516902896545330E+01, -1.649318291651622E+01, 3.262714764303443E-02, 7.348e22, ax))
+# Entity representing Venus
+orbital_entities.append(OrbitalEntity(-3.040772888668124E+05, -1.095389886846205E+08,-1.493038011771150E+06, 3.479834176174783E+01, -2.576663714548788E-03, -2.007354317633674E+00, 48.685e23, ax))
 
-print(orbital_entities[0])
-
-xpoints = np.array([])
-ypoints = np.array([])
-zpoints = np.array([])
-
-vxpoints = np.array([])
-vypoints = np.array([])
-vzpoints = np.array([])  
+print(orbital_entities[1])
 
 G = 6.6743e-20 # in km^3 / kg s^2
 dt = 86400 # in seconds
@@ -38,71 +42,92 @@ time_start = 0
 time_final = 15768000 * 2
 entity = orbital_entities[1]
 i = 0
-"""
-i = 0
-for t in range(0, time_final, dt):
-    xpoints = np.append(xpoints, [entity.x])
-    ypoints = np.append(ypoints, [entity.y])
-    vxpoints = np.append(vxpoints, [entity.vx])
-    vypoints = np.append(vypoints, [entity.vy])
-    print(f'{i}: (x: {entity.x}, y: {entity.y}) , (vx: {entity.vx}, vy: {entity.vy})')
 
-    # Drift
-    entity.x = entity.x + (entity.vx * dt)
-    entity.y = entity.y + (entity.vy * dt)
-
-    # Kick
-    rx = entity.x - orbital_entities[0].x
-    ry = entity.y - orbital_entities[0].y
-    r_mag = math.sqrt(rx * rx + ry * ry)
-    a = (-1) * G * orbital_entities[0].mass / (r_mag ** 2)
-    entity.vx = entity.vx + a * (rx / r_mag) * dt
-    entity.vy = entity.vy + a * (ry / r_mag) * dt
-    i+=1
-
-
-print(xpoints)
-print(ypoints)
-"""
-fig, ax = plt.subplots()
-ax = fig.add_subplot(projection='3d')
 ln, = ax.plot([], [], [])
+ln2, = ax.plot([], [], [])
+
+def all_entity_lines():
+    lines  = []
+    for e in orbital_entities:
+        lines.append(e.ln)
+    return tuple(lines)
 
 def init():
     ax.set_xlim(-149597871, 149597871)
     ax.set_ylim(-149597871, 149597871)
-    return ln,
+    ax.set_zlim(-149597871, 149597871)
+    return all_entity_lines()
 
 def update(frame):
-    global xpoints
-    global ypoints
-    global zpoints
-    global vxpoints
-    global vypoints
     t = frame
-    xpoints = np.append(xpoints, [entity.x])
-    ypoints = np.append(ypoints, [entity.y])
-    zpoints = np.append(zpoints, [0])
-    vxpoints = np.append(vxpoints, [entity.vx])
-    vypoints = np.append(vypoints, [entity.vy])
+    return multiple_orbit(t)
+
+def multiple_orbit(t):
+    # Drift (Move each orbital entity)
+    for i in range(0, len(orbital_entities)):
+        entity = orbital_entities[i]
+        # Keep track of all positions of an entity
+        entity.xpoints = np.append(entity.xpoints, [entity.x])
+        entity.ypoints = np.append(entity.ypoints, [entity.y])
+        entity.zpoints = np.append(entity.zpoints, [0])
+        entity.x = entity.x + (entity.vx * dt)
+        entity.y = entity.y + (entity.vy * dt)
+        entity.z = entity.z + (entity.vz * dt)
+        if (t == 86400):
+            print("Hello world: " , entity.x  , ", " , entity.y , ", " , entity.z)
+    # Kick
+    for i in range(0, len(orbital_entities)):
+        e = orbital_entities[i]
+        ax, ay, az = 0, 0, 0
+        for j in range(0, len(orbital_entities)):
+            e2 = orbital_entities[j]
+            if (i != j):
+                rx = e.x - e2.x
+                ry = e.y - e2.y
+                rz = e.z - e2.z
+                r_mag = math.sqrt(rx * rx + ry * ry + rz * rz)
+                a = (-1) * G * e2.mass / (r_mag ** 2)
+                ax += a * (rx / r_mag) * dt
+                ay += a * (ry / r_mag) * dt
+                az += a * (rz / r_mag) * dt
+        e.vx += ax
+        e.vy += ay
+        e.vz += az
+        
+        e.ln.set_data_3d(e.xpoints, e.ypoints, e.zpoints)
+    #print(orbital_entities[0].xpoints, " " , orbital_entities[0].ypoints)
+    return all_entity_lines()
+
+
+def single_orbit(t):
+    entity.xpoints = np.append(entity.xpoints, [entity.x])
+    entity.ypoints = np.append(entity.ypoints, [entity.y])
+    entity.zpoints = np.append(entity.zpoints, [0])
     print(f'(x: {entity.x}, y: {entity.y}) , (vx: {entity.vx}, vy: {entity.vy})')
 
     # Drift
     entity.x = entity.x + (entity.vx * dt)
     entity.y = entity.y + (entity.vy * dt)
+    entity.z = entity.z + (entity.vz * dt)
 
     # Kick
     rx = entity.x - orbital_entities[0].x
     ry = entity.y - orbital_entities[0].y
-    r_mag = math.sqrt(rx * rx + ry * ry)
+    rz = entity.z - orbital_entities[0].z
+    r_mag = math.sqrt(rx * rx + ry * ry + rz * rz)
     a = (-1) * G * orbital_entities[0].mass / (r_mag ** 2)
     entity.vx = entity.vx + a * (rx / r_mag) * dt
     entity.vy = entity.vy + a * (ry / r_mag) * dt
+    entity.vz = entity.vz + a * (rz / r_mag) * dt
 
-    ln.set_data_3d(xpoints, ypoints, zpoints)
-    return ln,
 
-ani = FuncAnimation(fig, update, frames=np.arange(time_start, time_final, dt), init_func=init, blit=True, interval=5, repeat=False)
+    ln.set_data_3d(entity.xpoints, entity.ypoints, entity.zpoints)
+    ln2.set_data_3d(entity.zpoints, entity.xpoints, entity.ypoints)
+    return ln, ln2,
+
+
+
+ani = FuncAnimation(fig, update, frames=np.arange(time_start, time_final, dt), init_func=init, blit=True, interval=15, repeat=False)
 #ax.plot(np.arange(0, time_final, dt), vxpoints, "b", label="vx")
 #ax.plot(np.arange(0, time_final, dt), vypoints, "r", label="vy")
 #ax.scatter(xpoints, ypoints)
