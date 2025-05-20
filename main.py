7619fec104b3e9cd6150040c2615119edb25a081
Samuel Tselnik <139260316+stselnik@users.noreply.github.com>
2025-05-19 20:16:43 -0400

    chore: Reduced Acceleration Calculations - Made minor change to simulation implementation. Still O(n^2) time complexity but now n(n-1)/2 rather than just n^2.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import math
+import time
 from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
 import numpy as np
@@ -19,6 +20,7 @@ class OrbitalEntity:
         self.ypoints = np.array([])
         self.zpoints = np.array([])
         self.ln, = ax.plot([], [], [], color,label=name)
+        self.a = [0, 0, 0]
 
     def __str__(self):
         return f"Position: ({self.x}, {self.y}, {self.z}), Velocity: ({self.vx}, {self.vy}, {self.vx}) , Mass: {self.mass}"
@@ -28,6 +30,13 @@ fig, ax = plt.subplots()
 ax = fig.add_subplot(projection='3d')
 
 orbital_entities = []
+# Keep track of execution time to debug
+process_times = np.array([]) 
+"""
+
+Avg Time for 365 day (1yr) per frame, 182 day trail simulation (No shared calculation for force):  0.04892499701492541
+
+"""
 
 def add_orbital_entity(name, horizon_id, mass, ax, color=""):
     # Fetch all values for 
@@ -58,19 +67,6 @@ add_orbital_entity("Uranus", 799, 86.813e23, ax, "#B9EEF1")
 add_orbital_entity("Neptune", 899, 102.409e24, ax, "#4B80E4")
 add_orbital_entity("Pluto", 999, 1.307e22, ax, "#FFC8B7")
 
-
-
-# Entity representing the Sun
-#orbital_entities.append(OrbitalEntity("Sun", 0, 0, 0, 0, 0, 0, 1.989e30, ax))
-# Entity representing Earth
-#orbital_entities.append(OrbitalEntity(-8.529440084914488E+07, -1.262070629279483E+08, 3.267467302957177E+04, 2.421505166691593E+01, -1.677450100101587E+01, 2.508322287999576E-04, 5.972e24, ax))
-# Entity representing the Moon
-#orbital_entities.append(OrbitalEntity(-8.519842463576744E+07, -1.265887274718351E+08,-2.688295040652156E+02, 2.516902896545330E+01, -1.649318291651622E+01, 3.262714764303443E-02, 7.348e22, ax))
-# Entity representing Venus
-#orbital_entities.append(OrbitalEntity(-3.040772888668124E+05, -1.095389886846205E+08,-1.493038011771150E+06, 3.479834176174783E+01, -2.576663714548788E-03, -2.007354317633674E+00, 48.685e23, ax))
-
-print(orbital_entities[1])
-
 G = 6.6743e-20 # in km^3 / kg s^2
 dt = 86400 # in seconds
 time_start = 0
@@ -98,6 +94,8 @@ def update(frame):
     return multiple_orbit(t)
 
 def multiple_orbit(t):
+    global process_times
+    s = time.process_time()
     for iteration in range(0, int(365 / 16)):
         # Drift (Move each orbital entity)
         for i in range(0, len(orbital_entities)):
@@ -115,26 +113,34 @@ def multiple_orbit(t):
             entity.z = entity.z + (entity.vz * dt)
 
         # Kick
+        # Clear previous acceleration values
+        for i in range (0, len(orbital_entities)):
+            orbital_entities[i].a = [0, 0, 0]
         for i in range(0, len(orbital_entities)):
             e = orbital_entities[i]
-            ax, ay, az = 0, 0, 0
-            for j in range(0, len(orbital_entities)):
-                e2 = orbital_entities[j]
-                if (i != j):
-                    rx = e.x - e2.x
-                    ry = e.y - e2.y
-                    rz = e.z - e2.z
-                    r_mag = math.sqrt(rx * rx + ry * ry + rz * rz)
-                    a = (-1) * G * e2.mass / (r_mag ** 2)
-                    ax += a * (rx / r_mag) * dt
-                    ay += a * (ry / r_mag) * dt
-                    az += a * (rz / r_mag) * dt
-            e.vx += ax
-            e.vy += ay
-            e.vz += az
-            
+            for j in range(i + 1, len(orbital_entities)):
+                e2 =orbital_entities[j]
+                rx = e.x - e2.x
+                ry = e.y - e2.y
+                rz = e.z - e2.z
+                r_mag = math.sqrt(rx * rx + ry * ry + rz * rz)
+                F = (-1) * G * e.mass * e2.mass / (r_mag ** 2)
+                # Update current entities accelerations (ax, ay, az)
+                e.a[0] += F / e.mass * (rx / r_mag) * dt
+                e.a[1] += F / e.mass * (ry / r_mag) * dt
+                e.a[2] += F / e.mass * (rz / r_mag) * dt
+                # Update acceleration on current selected companion
+                e2.a[0] += F / e2.mass * (-1 * rx / r_mag) * dt
+                e2.a[1] += F / e2.mass * (-1 * ry / r_mag) * dt
+                e2.a[2] += F / e2.mass * (-1 * rz / r_mag) * dt
+            e.vx += e.a[0]
+            e.vy += e.a[1]
+            e.vz += e.a[2]
             e.ln.set_data_3d(e.xpoints, e.ypoints, e.zpoints)
-        #print(orbital_entities[0].xpoints, " " , orbital_entities[0].ypoints)
+
+    e = time.process_time()
+    process_times = np.append(process_times, [e-s])
+    print("Avg Time: ", np.average(process_times) , " Current time: ",e - s)
     return all_entity_lines()
 
 
@@ -149,6 +155,5 @@ ani = FuncAnimation(fig, update, frames=np.arange(time_start, time_final * 166, 
 #ax.grid()
 ax.legend()
 plt.show()
-
 
 
